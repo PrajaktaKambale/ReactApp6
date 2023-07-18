343d742dda614a64cf26296aa1cf974bd8eda7d7
Prajakta <prajaktakamble42@gmail.com>
2023-07-19 00:13:03 +0530

    first commit

---
+++ src/app1.js
@@ -0,0 +1,10 @@
+class NotesApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Notes App</h1>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<NotesApp />, document.getElementById("app"));
